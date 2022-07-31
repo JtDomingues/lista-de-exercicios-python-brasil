6a7eb6f6c9f2d3c15c083d3555e2559ff77cafab
Josiberto <josibertotdomingues@gmail.com>
2022-07-31 08:56:29 -0300

    Solução exercício número 15

--- secao_01_estrutura_sequencial/ex_15_clt_onerosa.py
+++ secao_01_estrutura_sequencial/ex_15_clt_onerosa.py
@@ -27,3 +27,22 @@ Mostrar os resultados com duas casas decimais
 
 def calcular_assalto_no_salario():
     """Escreva aqui em baixo a sua solução"""
+
+    ganhos_por_hora = float(input(''))
+    horas_trabalhadas_no_mes = float(input(''))
+
+    salario_bruto = ganhos_por_hora * horas_trabalhadas_no_mes
+    imposto_de_renda = salario_bruto * (11/100)
+    inss = salario_bruto * (8/100)
+    sindicato = salario_bruto * (5/100)
+    salario_liquido = salario_bruto - imposto_de_renda - inss - sindicato
+
+
+    print(f'+ Salário Bruto : {salario_bruto:.2f}')
+    print(f'- IR (11%) : R$ {imposto_de_renda:.2f}')
+    print(f'- INSS (8%) : R$ {inss:.2f}')
+    print(f'- Sindicato ( 5%) : R$ {sindicato:.2f}')
+    print(f'= Salário Liquido : R$ {salario_liquido:.2f}')
+
+
+
