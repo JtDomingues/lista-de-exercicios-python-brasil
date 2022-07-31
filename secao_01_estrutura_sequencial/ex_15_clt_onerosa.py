9cfca2857ce91491e3e72134f236ad9ee8735702
Clara Santos <92645535+ClaraSantosmf@users.noreply.github.com>
2022-07-31 11:12:57 -0300

    Update ex_15_clt_onerosa.py
    Boaaa! Não existe nada de errado aqui está tudo ótimo! Uma sugestão é, ao trabalhar com porcentos, prefira sempre trabalhar com números inteiros. Então 11% significa 0.11, assim como 8 é 0.08 e 5 com 0.05. Porque existe menos riscos de que uma divisão como essa possa interferir no seu código.  Isso vai ajudar também quando for soma com porcento. Por exemplo, somar a = 100 +10%, use a = 100 * 1,10. Isso substitui a necessidade de fazer  a = 100 * 0.10 = 10, depois 100 + 10

--- secao_01_estrutura_sequencial/ex_15_clt_onerosa.py
+++ secao_01_estrutura_sequencial/ex_15_clt_onerosa.py
@@ -32,9 +32,9 @@ def calcular_assalto_no_salario():
     horas_trabalhadas_no_mes = float(input(''))
 
     salario_bruto = ganhos_por_hora * horas_trabalhadas_no_mes
-    imposto_de_renda = salario_bruto * (11/100)
-    inss = salario_bruto * (8/100)
-    sindicato = salario_bruto * (5/100)
+    imposto_de_renda = salario_bruto * 0.11
+    inss = salario_bruto * 0.08
+    sindicato = salario_bruto * 0.05
     salario_liquido = salario_bruto - imposto_de_renda - inss - sindicato
 
 
